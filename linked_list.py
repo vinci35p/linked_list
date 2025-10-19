584a8a480b9515215bc9382f36247db189cf023a
Vincent35p <vinsoyt4321@gmail.com>
2025-10-19 20:25:52 +0800

    created method for insert at beginning, end

--- linked_list.py
+++ linked_list.py
@@ -7,3 +7,21 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def insert_at_beginning(self,data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def insert_at_end(self,data):
+        new_node = Node(data)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
